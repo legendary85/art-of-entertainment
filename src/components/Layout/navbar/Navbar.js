86a51a508a704327748f7daa4f4a6767675b1387
Kourtney McCullough <kourtneyjmccullough@gmail.com>
2020-01-29 02:04:35 -0600

    changes to navbar for testing

--- src/components/Layout/navbar/Navbar.js
+++ src/components/Layout/navbar/Navbar.js
@@ -13,7 +13,7 @@ export class Navbar extends Component {
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav ml-auto">
-            <Link className="nav-item nav-link active" to="/">Hommmmme <span className="sr-only">(current)</span></Link>
+            <Link className="nav-item nav-link active" to="/">Home <span className="sr-only">(current)</span></Link>
             <Link className="nav-item nav-link" to="/about">About</Link>
             <Link className="nav-item nav-link" to="/packages">Packages</Link>
             <Link className="nav-item nav-link" to="/contact">Contact</Link>
